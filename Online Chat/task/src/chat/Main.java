f2adb0e19c79a0bf39582e63954a30391f6cd7fb
daddy <daddymercury@gmail.com>
2020-09-21 20:42:11 +0300

    Description Online chat is an application that lets you send and receive messages through the Internet. This project will show and explain the magic behind casually chatting with your online pals: you will work that magic yourself!
    The first stage of the project is pretty simple. Imagine that some people are messaging each other in a group chat. Your program should receive raw messages from the standard input, process them, and output only the text type messages ignoring the technical messages like when someone joined/left the chat.
    
    Each line of the input contains the person's name and the action this person performed. If the person performed the sent action, then the rest of the line after the keyword sent is the text of their message. Check out the output example to better understanding!
    
    Objectives
    In this stage, your program should satisfy the following conditions:
    
    Every line of the input contains the person's name and the action this person performed (sent, joined, left, connected, and disconnected).
    The person's name is a single word; there are no character restrictions for the person’s name.
    The sent messages match the pattern "Name sent Message".
    The messages in the output match the pattern "Name: Message".
    All other types of messages are not shown in the output.
    Your program will receive only correct raw messages.
    Example
    For the following input:
    
    Nick joined
    Nick sent Hello everyone!
    Nick sent Someone here?
    Josh joined
    Josh sent Hey Nick!
    Nick sent Hey Josh! I thought no one here...
    Josh sent Oh, I just joined, so..
    Nick sent OK
    Nick disconnected
    Josh sent Are you here?
    Nick connected
    Nick sent Yes, just bad connection
    Josh sent Alright
    Nick left
    Josh left
    The program should output:
    
    Nick: Hello everyone!
    Nick: Someone here?
    Josh: Hey Nick!
    Nick: Hey Josh! I thought no one was here...
    Josh: Oh, I just joined, so..
    Nick: OK
    Josh: Are you here?
    Nick: Yes, just bad connection
    Josh: Alright

--- Online Chat/task/src/chat/Main.java
+++ Online Chat/task/src/chat/Main.java
@@ -1,10 +1,36 @@
 package chat;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Main {
-    static int x = 10;
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        for (int x = 5; x < 5; x++) {
+        while (scanner.hasNextLine()) {
+            Message nextMessage = new Message(scanner.nextLine());
+            if ("sent".equals(nextMessage.action)) {
+                System.out.println((nextMessage.participantName + ": " + nextMessage.message).trim());
+            }
         }
-        System.out.print(x);
+    }
+}
+
+class Message {
+    String participantName;
+    String action;
+    String message;
+
+    Message(String rawMessage) {
+        List<String> fields = Arrays.stream(rawMessage.split("\\s+")).collect(Collectors.toList());
+        participantName = fields.get(0);
+        action = fields.get(1);
+        StringBuilder messageBuilder = new StringBuilder();
+        for (int i = 2; i < fields.size(); i++) {
+            messageBuilder.append(fields.get(i)).append(" ");
+        }
+        message = messageBuilder.toString();
     }
 }
